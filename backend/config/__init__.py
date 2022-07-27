e708c2b9492230f2906fdeec99f3c2a5a7d76b82
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-07-28 00:26:43 +0100

    chore: added app config

---
+++ backend/config/__init__.py
@@ -0,0 +1,20 @@
+import os
+from pydantic import BaseSettings
+import dotenv
+
+dotenv.load_dotenv()
+
+
+class AppSettings(BaseSettings):
+
+    APP_NAME = "AlgoVerify API"
+    DEBUG_MODE = os.getenv("DEBUG", "True") == "True"
+    ALGO_INDEXER_URL = os.getenv("ALGO_INDEXER_URL")
+    ALGO_NODE_URL = os.getenv("ALGO_NODE_URL")
+    DATABASE_URI = os.getenv("DATABASE_URI")
+    DB_NAME = os.getenv("DB_NAME")
+    HOST = os.getenv("HOST")
+    PORT = int(os.getenv("PORT"))
+
+
+settings = AppSettings()
